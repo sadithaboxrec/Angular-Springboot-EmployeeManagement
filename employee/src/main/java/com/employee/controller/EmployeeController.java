1b5ccb4e67740da6916c8bde263029aa21e53f57
Saditha Fernando <sadithafernandods2002@gmail.com>
2026-01-04 20:44:45 +0530

    Background for security

--- employee/src/main/java/com/employee/controller/EmployeeController.java
+++ employee/src/main/java/com/employee/controller/EmployeeController.java
@@ -4,32 +4,38 @@ import com.employee.entity.Employee;
 import com.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 @CrossOrigin(maxAge = 3600)
 @RestController
+@RequestMapping("/api/v1/employees")
 public class EmployeeController {
 
     @Autowired
     private EmployeeService employeeService;
 
-    @GetMapping("/api/v1/employees")
+    @PreAuthorize("hasRole('MANAGER')")
+    @GetMapping
     public ResponseEntity< List<Employee>> getAllEmployees() {
         return ResponseEntity.ok( employeeService.getAllEmployees() );
     }
 
-    @PostMapping("/api/v1/employees")
+    @PreAuthorize("hasRole('MANAGER')")
+    @PostMapping
     public ResponseEntity <Employee> createEmployees(@RequestBody Employee employee) {
         return ResponseEntity.ok( employeeService.createEmployee(employee) );
     }
 
-    @PutMapping("/api/v1/employees/{id}")
+    @PreAuthorize("hasRole('MANAGER')")
+    @PutMapping("/{id}")
     public ResponseEntity <Employee> updateEmployee(@PathVariable("id") Integer id, @RequestBody Employee employee) {
         return ResponseEntity.ok(employeeService.updateEmployee(employee) );
     }
 
-    @DeleteMapping("/api/v1/employees/{id}")
+    @PreAuthorize("hasRole('MANAGER')")
+    @DeleteMapping("/{id}")
     public ResponseEntity <String> deleteEmployee(@PathVariable("id") Integer id, @RequestBody Employee employee) {
         return ResponseEntity.ok(employeeService.deleteEmployee(id) );
     }
